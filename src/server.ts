adacc3cd2ce97dd1406d736d1cfca7cb87e5690f
Davi Mateus <davi.2020109493@unicap.br>
2023-08-16 12:39:57 -0300

    setup: add typescript and fastify

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+
+const server = fastify();
+
+server.get("/hello", () => {
+  return "world";
+});
+
+server.listen({ port: 3333 }).then(() => {
+  console.log("Server listening on port 3333");
+});
