13673ec042cc4b8c6600a7948f1c2c909c3b8b7a
Davi Mateus <davi.2020109493@unicap.br>
2023-08-16 13:40:30 -0300

    config: eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const server = fastify();
+const server = fastify()
 
-server.get("/hello", () => {
-  return "world";
-});
+server.get('/hello', () => {
+  return 'world'
+})
 
 server.listen({ port: 3333 }).then(() => {
-  console.log("Server listening on port 3333");
-});
+  console.log('Server listening on port 3333')
+})
